0fe4ca00eec67f7781b14a6b4bb7a917d11f8d65
Amar Deep Singh <50517840+amardeep2006@users.noreply.github.com>
2023-11-24 09:28:45 +0530

    Create bootstrapKINDTests.sh

---
+++ tests/bootstrapKINDTests.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+cd tests || true
+
+if [ "${CI:-false}" = "false" ]; then
+  pip3 install virtualenv | grep -v 'Requirement already satisfied'
+  virtualenv docker-selenium-tests
+  source docker-selenium-tests/bin/activate
+fi
+
+python -m pip install selenium==4.15.0 \
+                      docker===6.1.3 \
+                      | grep -v 'Requirement already satisfied'
+
+python testOnGrid.py
+ret_code=$?
+
+if [ "${CI:-false}" = "false" ]; then
+  deactivate
+fi
+
+exit $ret_code
